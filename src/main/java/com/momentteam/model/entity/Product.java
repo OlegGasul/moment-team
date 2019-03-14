a53cb05584b51ed4f2bed005bad15955f448caee
Oleg Gasiul <oleg.g@yggdrasilgaming.com>
2019-03-14 22:57:45 +0100

    Added constructors

--- src/main/java/com/momentteam/model/entity/Product.java
+++ src/main/java/com/momentteam/model/entity/Product.java
@@ -2,31 +2,33 @@ package com.momentteam.model.entity;
 
 import java.math.BigDecimal;
 import java.util.Set;
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "product")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
   @Id
-  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  @Column(name = "name")
   private String name;
 
-  @Column(name = "number")
   private String number;
 
-  @Column(name = "price")
   private BigDecimal price;
 
   @OneToMany(mappedBy = "product")
--- src/main/java/com/momentteam/model/entity/Sale.java
+++ src/main/java/com/momentteam/model/entity/Sale.java
@@ -4,31 +4,39 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "sale")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Sale {
 
   @Id
   @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  @Column(name = "price")
   private BigDecimal price;
 
-  @Column(name = "quantity")
   private Long quantity;
 
-  @Column(name = "discount")
   private BigDecimal discount;
 
   @Column(name = "sale_date")
   private LocalDateTime saleDate;
+
+  @ManyToOne
+  private Product product;
 
 }
