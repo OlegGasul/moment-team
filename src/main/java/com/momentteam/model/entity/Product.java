b908b9e41a990f08e62d829a5f32a195196b683b
Oleg Gasiul <oleg.g@yggdrasilgaming.com>
2019-03-14 15:32:52 +0100

    Schema, repo, readme

--- src/main/java/com/momentteam/model/entity/Product.java
+++ src/main/java/com/momentteam/model/entity/Product.java
@@ -1,8 +1,5 @@
 package com.momentteam.model.entity;
 
-import static com.momentteam.model.entity.Product.GET_ALL;
-import static com.momentteam.model.entity.Product.GET_PRODUCT_BY_ID;
-
 import java.math.BigDecimal;
 import java.util.Set;
 import javax.persistence.Column;
@@ -12,17 +9,12 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
-import org.hibernate.annotations.NamedQuery;
 
 @Entity
 @Table(name = "product")
 @Data
 @Builder
-@NamedQuery(name = GET_ALL, query = "select p from Product")
-@NamedQuery(name = GET_PRODUCT_BY_ID, query = "select p from Product where id = ?")
 public class Product {
-  public static final String GET_ALL = "getAllProducts";
-  public static final String GET_PRODUCT_BY_ID = "getProductById";
 
   @Id
   @Column(name = "id")
