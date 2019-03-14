faaf5592e081a9805efafc170bf55c36ed45f101
Oleg Gasiul <oleg.g@yggdrasilgaming.com>
2019-03-14 01:26:26 +0100

    Skeleton project

--- src/main/java/com/momentteam/controller/ProductController.java
+++ src/main/java/com/momentteam/controller/ProductController.java
@@ -5,13 +5,16 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import com.momentteam.model.ProductConverter;
 import com.momentteam.model.dto.ProductDto;
+import com.momentteam.model.entity.Product;
 import com.momentteam.service.ProductService;
+import java.util.Optional;
 import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 @RestController
 @RequestMapping("/product")
@@ -26,12 +29,13 @@ public class ProductController {
 
   @RequestMapping(method = POST, path = "/")
   public ProductDto add(@RequestBody ProductDto productDto) {
-    return productConverter.convert(productService.createProduct());
+    throw new NotImplementedException();
   }
 
   @RequestMapping(method = GET, path = "/{id}")
   public ProductDto get(@PathVariable(value = "name") @NotNull long id) {
-    return productConverter.convert(productService.get(id));
+    Optional<Product> optionalProduct = productService.findById(id);
+    return optionalProduct.isPresent() ? productConverter.convert(optionalProduct.get()) : null;
   }
 
 }
